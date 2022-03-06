384e270f663c4dd9cc4120cc2ed61319eb3de06d
pajith551@gmail.com <pajith551@gmail.com>
2022-03-06 22:34:11 +0530

    intro to node

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+const superheroes = require("superheroes");
+
+const myHeroes = superheroes.random();
+console.log(myHeroes);
+
+const supervillains = require("superheroes");
+
+// const myVillains = supervillains.all;
+const myVillainsRandom = supervillains.random();
+// console.log(myVillains);
+console.log(myVillainsRandom);
